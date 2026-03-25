166577690372c904036edd89da042cbf82c0b9f8
jkholick <jauharkuttyk3@gmail.com>
2026-03-25 20:51:25 +0530

    Feat (fish): Changing shell to fish and shifting aliases and plugins

---
+++ .config/fish/conf.d/plugins.fish
@@ -0,0 +1,11 @@
+fundle plugin 'edc/bass'
+fundle plugin 'ilancosman/tide@v6'
+fundle plugin 'patrickf1/fzf.fish'
+fundle plugin 'meaningful-ooo/sponge'
+fundle plugin 'nickeb96/puffer-fish'
+fundle plugin 'jorgebucaran/autopair.fish'
+
+fundle init
+
+
+zoxide init fish | source
